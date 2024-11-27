26b7f957cafd908149be3f23b667c67be27814cb
Tufelux <126290164+Tufelux@users.noreply.github.com>
2024-11-27 12:30:09 +0000

    Finished game code so it's playable

--- script.js
+++ script.js
@@ -13,23 +13,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let input;
+    let input = null;
     while (input !== "rock" && input !== "paper" && input !== "scissors") {
         input = prompt("What do you choose to throw out?");
+        if (input === null) {
+            console.log("Game canceled by the user.");
+            return null;
+        }
     }
     return input;
-    
 }
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+
+    let result;
+
+    let humanScore = 0;
+    let computerScore = 0;
     
+    while (humanScore !== 5 && computerScore !== 5) {
+        
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        if (humanChoice === computerChoice) {
+            console.log("Aww it's a draw!");
+        }
+        else {
+            switch (humanChoice) {
+                case "rock":
+                    result = (computerChoice === "scissors") ? ("You win!") : ("You lose!");
+                    break;
+                case "paper":
+                    result = (computerChoice === "rock") ? ("You win!") : ("You lose!");
+                    break;
+                case "scissors":
+                    result = (computerChoice === "paper") ? ("You win!") : ("You lose!");
+                    break;
+            }
+            console.log(result);
+            if (result === "You win!") {
+                humanScore++;
+            } else {
+                computerScore++;
+            }
+        }
+    }
+    console.log(humanScore === 5 ? "You won!" : "The computer won ):");
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
-
-console.log(x);
-console.log(y);
+playGame();
